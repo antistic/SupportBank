189173d44592f547d7585f08f699ca7a31e3c301
Antonia Siu <Antonia.Siu@softwire.com>
2018-07-11 09:47:26 +0100

    Basic command structure

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -10,6 +10,45 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
+            string input = "";
+            while (!input.Equals("q"))
+            {
+                Console.WriteLine("Enter a command");
+                input = Console.ReadLine();
+                if (!validCommand(input))
+                {
+                    Console.WriteLine("Command must be 'List All', 'List [Account]' or 'q' to quit");
+                }
+                else
+                {
+                    string target = input.Split(' ')[1];
+                    switch (target)
+                    {
+                        case "All":
+                            Console.WriteLine("listing all");
+                            break;
+                        default:
+                            // TODO: check if valid account
+                            Console.WriteLine("listing the account " + target);
+                            break;
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            C
+        }
+
+        static bool validCommand(string command)
+        {
+            string[] split = command.Split(' ');
+            if (split.Length == 2 && split[0].Equals("List"))
+            {
+                // TODO: check if valid account/"All"
+                return true;
+            }
+
+            return false;
         }
     }
 }
